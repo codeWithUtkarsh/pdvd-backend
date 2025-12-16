8a06abbfea849fec0edc0ba9a8a6728a43c75211
Steve Taylor <steve@deployhub.com>
2025-12-16 09:16:51 -0700

    refactor helpers

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/google/osv-scanner v1.9.2
 	github.com/graphql-go/graphql v0.8.1
 	github.com/package-url/packageurl-go v0.1.3
+	github.com/pandatix/go-cvss v0.6.2
 	go.uber.org/zap v1.27.1
 	gopkg.in/yaml.v2 v2.4.0
 )
@@ -44,5 +45,4 @@ require (
 	golang.org/x/sys v0.39.0 // indirect
 	golang.org/x/text v0.32.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
-	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
---
+++ util/version.go
@@ -0,0 +1,17 @@
+// Package util provides utility functions for the backend.
+package util
+
+import (
+	"github.com/google/osv-scanner/pkg/models"
+)
+
+// IsVersionAffectedAny checks if a version is affected by any of the provided affected ranges
+// This is a convenience wrapper around IsVersionAffected for multiple affected entries
+func IsVersionAffectedAny(version string, allAffected []models.Affected) bool {
+	for _, affected := range allAffected {
+		if IsVersionAffected(version, affected) {
+			return true
+		}
+	}
+	return false
+}
